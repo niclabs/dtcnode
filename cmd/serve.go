88f359e6ddedfdaa8b607d1d7959319cde42d245
Eduardo <yo+gitea@adderou.cl>
2019-08-30 00:57:19 -0400

    now config is required only if serve is executed

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,14 +1,22 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/niclabs/dtcnode/server"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Runs the node",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		viper.SetConfigName("config")
+		viper.AddConfigPath("/etc/dtcnode/")
+		viper.AddConfigPath("./")
+		if err := viper.ReadInConfig(); err != nil {
+			panic(fmt.Errorf("config file not found! %v", err))
+		}
 		return server.Serve()
 	},
 }
--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/niclabs/dtcnode/cmd"
-	"github.com/spf13/viper"
 )
-
-func init() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath("/etc/dtcnode/")
-	viper.AddConfigPath("./")
-	if err := viper.ReadInConfig(); err != nil {
-		panic(fmt.Errorf("config file not found! %v", err))
-	}
-}
-
 
 func main() {
 	cmd.Execute()
